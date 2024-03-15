9958dd3cb58f121238005398767ed9bfb23fa733
subinprk <subpark@c4b12c3.42berlin.de>
2024-03-15 20:41:01 +0100

    gonna go home

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,20 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 20:19:41 by subpark           #+#    #+#             */
-/*   Updated: 2024/03/15 20:19:43 by subpark          ###   ########.fr       */
+/*   Updated: 2024/03/15 20:40:51 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+std::string ClapTrap::get_name()
+{
+    return (_name);
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+    std::cout << _name <<" attacks "<< target <<", causing"
+        << ;
+    
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,27 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 20:19:26 by subpark           #+#    #+#             */
-/*   Updated: 2024/03/15 20:19:39 by subpark          ###   ########.fr       */
+/*   Updated: 2024/03/15 20:39:21 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define CLAPTRAP_CPP
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string _name;
+        int         _hit_points;
+        int         _energy_points;
+        int         _attack_damage;
+    public:
+        void attack(const std::string &target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+        ClapTrap(const std::string name): _name(name), _hit_points(10),
+                            _energy_points(10), _attack_damage(10);
+        ClapTrap() = default;
+        std::string get_name();
+};
